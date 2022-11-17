f2baf1470143ea56c262d9ba0db65ca0aebcb5ae
Polzocr <micro.cpt.62@outlook.fr>
2022-11-17 12:44:32 +0100

    ajout des elements dropdowns

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,26 +1,38 @@
 const ingre = document.querySelector('.dropdown__search.ingredients')
 ingre.addEventListener('click', function(e) {
     this.removeAttribute('value')
+    this.nextElementSibling.classList.add('show')
+    this.nextElementSibling.classList.add('ingredients')
 })
 
 ingre.addEventListener('focusout', function(e) {
     this.setAttribute('value', 'Ingrédients')
+    this.nextElementSibling.classList.remove('show')
+    this.nextElementSibling.classList.remove('ingredients')
 })
 
 const app = document.querySelector('.dropdown__search.appareil')
 app.addEventListener('click', function(e) {
     this.removeAttribute('value')
+    this.nextElementSibling.classList.add('show')
+    this.nextElementSibling.classList.add('appareil')
 })
 
 app.addEventListener('focusout', function(e) {
     this.setAttribute('value', 'Ingrédients')
+    this.nextElementSibling.classList.remove('show')
+    this.nextElementSibling.classList.remove('appareil')
 })
 
 const ust = document.querySelector('.dropdown__search.ustensile')
 ust.addEventListener('click', function(e) {
     this.removeAttribute('value')
+    this.nextElementSibling.classList.add('show')
+    this.nextElementSibling.classList.add('ustensile')
 })
 
 ust.addEventListener('focusout', function(e) {
     this.setAttribute('value', 'Ingrédients')
+    this.nextElementSibling.classList.remove('show')
+    this.nextElementSibling.classList.remove('ustensile')
 })
